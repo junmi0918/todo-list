509835919896b0cfcff3d58db37449684c010468
junmi0918 <78129272+junmi0918@users.noreply.github.com>
2022-12-06 03:07:08 +0900

    Update weatheer.js

--- js/weatheer.js
+++ js/weatheer.js
@@ -3,7 +3,7 @@ const API_KEY = "4ea02e8a6f01b304c7cd90473752b704";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;;
     
     fetch(url)
     .then(response => response.json())
